788279e7ab264ad4647d3c5a5de72c4be35c272c
Brian <blsvsanteliz@gmail.com>
2021-04-18 19:49:03 -0400

    Create getStaticProps function and map index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import styles from '../styles/Home.module.css';
-import Link from 'next/link';
-import { persons } from './data.json';
-import Meta from '../components/Meta';
-const Home = () => {
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { persons } from "./data.json";
+import Meta from "../components/Meta";
+import useSWR from "swr";
+
+const Home = ({ data }) => {
   return (
     <>
       <Meta />
@@ -13,19 +15,49 @@ const Home = () => {
           <a className={styles.linkStyle}>About</a>
         </Link>
       </div>
-      {persons.map((e) => {
-        return (
-          <ul key={e.name}>
-            <li>
-              <strong>name</strong>:{e.name}
-            </li>
-            <li>
-              <strong>lastname</strong>:{e.lastname}
-            </li>
-          </ul>
-        );
-      })}
+      <div>
+        <h1>Post from API</h1>
+        {data.map((post) => {
+          return (
+            <div key={post.id}>
+              <Link href={`/post/${post.id}`}>
+                <h2 className="post">{post.title}</h2>
+              </Link>
+              <p>{post.body}</p>
+            </div>
+          );
+        })}
+        {persons.map((e) => {
+          return (
+            <ul key={e.name}>
+              <li>
+                <strong>name</strong>:{e.name}
+              </li>
+              <li>
+                <strong>lastname</strong>:{e.lastname}
+              </li>
+            </ul>
+          );
+        })}
+      </div>
     </>
   );
 };
+export async function getStaticProps() {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: { data },
+  };
+}
 export default Home;
